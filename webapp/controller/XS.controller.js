aad5e3c34acd074bce27a304bea790ba5519eb8c
Fabio Pagoti <fabio.pagoti@ovly.com.br>
2018-09-19 21:26:56 +0000

    asd

--- webapp/controller/XS.controller.js
+++ webapp/controller/XS.controller.js
@@ -4,6 +4,12 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("alfaerp.template.Template.controller.XS", {
-
+		onInit: function(){
+			window.load = this._load;
+		},
+		
+		_load: function(){
+			// /alfaerp/util/serviceLayer.xsjs?cmd=Get&actionUri=Banks&sessionID=c36a2bd8-bc45-11e8-8000-00155d0f1300&username=B1SiteUser&filter=BankCode eq 267
+		}
 	});
 });
